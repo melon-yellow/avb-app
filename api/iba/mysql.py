61baf6f76a7982cdbc917a723c34144ac82703bd
anthony-freitas <78743473+anthony-freitas@users.noreply.github.com>
2021-10-24 23:51:06 -0300

    rewrwer

--- api/iba/mysql.py
+++ api/iba/mysql.py
@@ -3,78 +3,72 @@
 
 # Imports
 import os
-import pyodbc
+import pandas
+import mysql.connector
+import datetime
+
+# Modules
+from .. import turno
 
 #################################################################################################################################################
 
 # Get File-Paths
 fileDir = os.path.dirname(__file__)
-product_sql = os.path.join(fileDir, '..\\sql\\iba.mssql.product.sql')
-rfa_lim_sql = os.path.join(fileDir, '..\\sql\\iba.mssql.rfa.lim.sql')
-rfa_sql = os.path.join(fileDir, '..\\sql\\iba.mssql.rfa.sql')
+util_sql = os.path.join(fileDir, '..\\sql\\trefila.util.sql')
+util_day_sql = os.path.join(fileDir, '..\\sql\\trefila.util.dia.sql')
 
 #################################################################################################################################################
 
-    @api.route('/api/util_csv/')
-    def trf_util_csv(req: Request, res: Response):
-        # MySQL Connection
-        mydb = mysql.connector.connect(
-            host='127.0.0.1',
-            user='Jayron',
-            passwd='123456',
-            port='3306',
-            database='iba_i'
-        )
-        sql = open('../sql/trefila.util.sql').read()
-        df = pandas.read_sql(sql, mydb)
-        mydb.close()
-        # Processing
-        meses = {'1':[1,2,3],'2':[4,5,6],'3':[7,8,9],'4':[10,11,12]}
-        df['_0h'] = df._date.apply(lambda row : turno.escala.get(dia = row)[0][0])
-        df['_8h'] = df._date.apply(lambda row : turno.escala.get(dia = row)[1][0])
-        df['_16h'] = df._date.apply(lambda row : turno.escala.get(dia = row)[2][0])
-        df['_date'] = df['_date'].astype('str')
-        dz = df
-        hoje = py_misc.datetime.date.today()
-        #hoje = datetime.datetime.now()
-        #hoje = datetime.date(2021,2,23)
-        Trimestre = str((hoje.month-1)//3+1)
-        Tmeses = meses.get(Trimestre)
-        #mes = datetime.date(2021,3,23).month
-        bn = df[df['_date'] >= py_misc.datetime.date(hoje.year, Tmeses[0], 1).strftime("%Y-%m-%d")]
-        bn = bn[bn['_date'] <= hoje.strftime("%Y-%m-%d")]
-        bn = bn.filter(['_date','M1','M2','M3','M4','M5','_0h','_8h','_16h'])
-        bn = bn.drop(['M1'], axis=1)
-        bn['Global'] = (bn['M2']+bn['M3']+bn['M4']+bn['M5']) / 4
-        # Retrun Data
-        return res(
-            bn.to_csv(),
-            mimetype = "text/csv",
-            headers = {
-                "Content-disposition": "attachment; filename=utilizacao.csv"
-            }
-        )
+def UtilizacaoTrefila():
+    # MySQL Connection
+    mydb = mysql.connector.connect(
+        host='127.0.0.1',
+        user='Jayron',
+        passwd='123456',
+        port='3306',
+        database='iba_i'
+    )
+    df = pandas.read_sql(
+        open(util_sql).read(),
+        mydb
+    )
+    mydb.close()
+    # Processing
+    meses = {'1':[1,2,3],'2':[4,5,6],'3':[7,8,9],'4':[10,11,12]}
+    df['_0h'] = df._date.apply(lambda row : turno.escala.get(dia = row)[0][0])
+    df['_8h'] = df._date.apply(lambda row : turno.escala.get(dia = row)[1][0])
+    df['_16h'] = df._date.apply(lambda row : turno.escala.get(dia = row)[2][0])
+    df['_date'] = df['_date'].astype('str')
+    hoje = datetime.date.today()
+    Trimestre = str((hoje.month-1)//3+1)
+    Tmeses = meses.get(Trimestre)
+    bn = df[df['_date'] >= datetime.date(hoje.year, Tmeses[0], 1).strftime("%Y-%m-%d")]
+    bn = bn[bn['_date'] <= hoje.strftime("%Y-%m-%d")]
+    bn = bn.filter(['_date','M1','M2','M3','M4','M5','_0h','_8h','_16h'])
+    bn = bn.drop(['M1'], axis=1)
+    bn['Global'] = (bn['M2']+bn['M3']+bn['M4']+bn['M5']) / 4
+    # Retrun Data
+    return str(bn.to_csv())
 
-    #################################################################################################################################################
+#################################################################################################################################################
 
-    @api.route('/api/util_csv_dia')
-    def trf_util_csv_dia(req: Request, res: Response):
-        # MySQL Connection
-        mydb = mysql.connector.connect(
-            host='127.0.0.1',
-            user='Jayron',
-            passwd='123456',
-            port='3306',
-            database='iba_i'
-        )
-        sql = open('sql/trf_util_day.sql').read()
-        csv = pandas.read_sql(sql, mydb).to_csv()
-        mydb.close()
-        # Return Data
-        return res(
-            csv,
-            mimetype = "text/csv",
-            headers = {
-                "Content-disposition": "attachment; filename=utilizacao-dia.csv"
-            }
-        )
+def UtilizacaoDiaTrefila():
+    # MySQL Connection
+    mydb = mysql.connector.connect(
+        host='127.0.0.1',
+        user='Jayron',
+        passwd='123456',
+        port='3306',
+        database='iba_i'
+    )
+    csv = str(
+        pandas.read_sql(
+            open(util_day_sql).read(),
+            mydb
+        ).to_csv()
+    )
+    mydb.close()
+    # Return Data
+    return csv
+
+#################################################################################################################################################
--- api/routes/trefila.py
+++ api/routes/trefila.py
@@ -3,7 +3,6 @@
 
 # Imports
 import py_misc
-import mysql.connector
 import pandas
 import io
 
@@ -11,6 +10,7 @@ import io
 from .. import turno
 from .. import homerico
 from .. import metas
+from .. import iba
 
 #################################################################################################################################################
 
@@ -149,37 +149,10 @@ def __load__(api: py_misc.API):
     @api.route('/api/util_csv/')
     def trf_util_csv(req: Request, res: Response):
         # MySQL Connection
-        mydb = mysql.connector.connect(
-            host='127.0.0.1',
-            user='Jayron',
-            passwd='123456',
-            port='3306',
-            database='iba_i'
-        )
-        sql = open('../sql/trefila.util.sql').read()
-        df = pandas.read_sql(sql, mydb)
-        mydb.close()
-        # Processing
-        meses = {'1':[1,2,3],'2':[4,5,6],'3':[7,8,9],'4':[10,11,12]}
-        df['_0h'] = df._date.apply(lambda row : turno.escala.get(dia = row)[0][0])
-        df['_8h'] = df._date.apply(lambda row : turno.escala.get(dia = row)[1][0])
-        df['_16h'] = df._date.apply(lambda row : turno.escala.get(dia = row)[2][0])
-        df['_date'] = df['_date'].astype('str')
-        dz = df
-        hoje = py_misc.datetime.date.today()
-        #hoje = datetime.datetime.now()
-        #hoje = datetime.date(2021,2,23)
-        Trimestre = str((hoje.month-1)//3+1)
-        Tmeses = meses.get(Trimestre)
-        #mes = datetime.date(2021,3,23).month
-        bn = df[df['_date'] >= py_misc.datetime.date(hoje.year, Tmeses[0], 1).strftime("%Y-%m-%d")]
-        bn = bn[bn['_date'] <= hoje.strftime("%Y-%m-%d")]
-        bn = bn.filter(['_date','M1','M2','M3','M4','M5','_0h','_8h','_16h'])
-        bn = bn.drop(['M1'], axis=1)
-        bn['Global'] = (bn['M2']+bn['M3']+bn['M4']+bn['M5']) / 4
+        csv = iba.mysql.UtilizacaoTrefila()
         # Retrun Data
         return res(
-            bn.to_csv(),
+            csv,
             mimetype = "text/csv",
             headers = {
                 "Content-disposition": "attachment; filename=utilizacao.csv"
@@ -191,16 +164,7 @@ def __load__(api: py_misc.API):
     @api.route('/api/util_csv_dia')
     def trf_util_csv_dia(req: Request, res: Response):
         # MySQL Connection
-        mydb = mysql.connector.connect(
-            host='127.0.0.1',
-            user='Jayron',
-            passwd='123456',
-            port='3306',
-            database='iba_i'
-        )
-        sql = open('sql/trf_util_day.sql').read()
-        csv = pandas.read_sql(sql, mydb).to_csv()
-        mydb.close()
+        csv = iba.mysql.UtilizacaoDiaTrefila()
         # Return Data
         return res(
             csv,
