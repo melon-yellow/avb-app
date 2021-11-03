510214a3fffeee999e8f0aa8924d1f468c6c5ec1
anthony-freitas <78743473+anthony-freitas@users.noreply.github.com>
2021-11-03 10:03:35 -0300

    fixing cx_Oracle

--- app/furl2/oracle.py
+++ app/furl2/oracle.py
@@ -15,14 +15,19 @@ gusaapp_sql = os.path.abspath(os.path.join(fileDir, '../sql/furl2.oracle.gusaapp
 def gusaapp():
     r = None
     try:
-        conn = cx_Oracle.connect('gusaapp/gusaapp@10.20.6.66/orcl')
+        conn = cx_Oracle.connect(
+            dsn=os.getenv('ORACLE_DB_FURNACE_DSN'),
+            user=os.getenv('ORACLE_DB_FURNACE_USER'),
+            password=os.getenv('ORACLE_DB_FURNACE_PASSWORD'),
+            encoding='UTF-8'
+        )
         cur = conn.cursor()
         cur.execute(open(gusaapp_sql).read())
         r = [dict((cur.description[i][0], value)
             for i, value in enumerate(row)) for row in cur.fetchall()]
         r = r[0] if len(r) > 0 else dict()
         cur.connection.close()
-    except: r = None
+    except: pass
     return r
 
 #################################################################################################################################################
