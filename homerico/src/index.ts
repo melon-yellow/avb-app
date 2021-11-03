20ba67b4deca7ce6ed95fe01fa21fca1c94c08e3
anthony-freitas <78743473+anthony-freitas@users.noreply.github.com>
2021-11-03 05:21:16 -0300

    j asuncizicizicizx

--- homerico/src/index.ts
+++ homerico/src/index.ts
@@ -17,8 +17,17 @@
 // Import Homerico
 import HomericoConexao from 'ts-homerico'
 
+// Import Super-Guards
+import Miscellaneous from 'ts-misc'
+import { is } from 'ts-misc/dist/utils/guards.js'
+
 // Import Express
-import express from 'express'
+import express, { RequestHandler } from 'express'
+import basicAuth from 'express-basic-auth'
+import requestIp from 'request-ip'
+
+// Instance Miscellaneous
+const misc = new Miscellaneous()
 
 /*
 ##########################################################################################################################
@@ -35,6 +44,63 @@ homerico.login({
   usuario: process.env.HOMERICO_USER,
   senha: process.env.HOMERICO_PASSWORD
 })
+
+/*
+##########################################################################################################################
+#                                                          MAIN                                                          #
+##########################################################################################################################
+*/
+
+// Set App
+const app = express()
+app.use(express.json() as RequestHandler)
+
+// Homerico Ignore Items
+const ignore = ['acesso', 'relatorio', 'validar', 'login'] as const
+
+// Iterate over Homerico Methods
+for (const item in homerico) {
+  // Check for Valid Item
+  if (item in ignore) continue
+  // Post Endnode
+  app.post(`/homerico/${item}`, async (req, res) => {
+    // log action to be executed
+    const ip = requestIp.getClientIp(req).replace('::ffff:', '')
+    misc.log(`Exec(homerico::${item}) From(${ip})`)
+    // check request
+    if (!is.object(req)) throw new Error('bad request')
+    if (!is.object(req.body)) throw new Error('bad request')
+    // Execute Function
+    const data = homerico[item](req.body)
+    // Send Response
+    res.send(data)
+  })
+}
+
+/*
+##########################################################################################################################
+#                                                          MAIN                                                          #
+##########################################################################################################################
+*/
+
+// Get Listen Port
+const port = process.env.HOMERICO_NETWORK_PORT
+
+// Get Authentication
+const [user, password] = [
+  process.env.HOMERICO_NETWORK_USER,
+  process.env.HOMERICO_NETWORK_PASSWORD
+]
+
+// Set Authentication
+app.use(
+  basicAuth({
+    users: { [user]: password }
+  })
+)
+
+// Listen on Port Especified
+app.listen(port)
 
 /*
 ##########################################################################################################################
