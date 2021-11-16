c28e750898b0d174ed6b093e8f9854fe4d32f4a6
anthony <78743473+anthony-freitas@users.noreply.github.com>
2021-11-16 12:46:25 -0300

    dsfsdfsdfsdf

--- app/routes/sap.py
+++ app/routes/sap.py
@@ -23,16 +23,23 @@ def __load__(app: py_misc.Express):
 
     @app.route('/avb/sap/preditivas/')
     def sapPreditivas(req: Request, res: Response):
-        input = req.json()
-        # Check Input
-        if not isinstance(input, dict): raise Exception('bad request')
-        if 'equip' not in input: raise Exception('invalid argument "equip"')
-        # Get Data
-        data = odbc.sap.preditivas(input['equip'])
-        return res(
-            json.dumps(data),
-            mimetype='application/json',
-            status=200
-        )
+        try:
+            input = req.json()
+            # Check Input
+            if not isinstance(input, dict): raise Exception('bad request')
+            if 'equip' not in input: raise Exception('invalid argument "equip"')
+            # Get Data
+            data = odbc.sap.preditivas(input['equip'])
+            return res(
+                json.dumps(data),
+                mimetype='application/json',
+                status=200
+            )
+        except Exception as e:
+            return res(
+                json.dumps({ 'error': str(e) }),
+                mimetype='application/json',
+                status=200
+            )
 
 #################################################################################################################################################
