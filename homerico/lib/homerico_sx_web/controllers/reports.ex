f8fcc7622140dd3811a1a455c80102673ad8d49b
anthony <78743473+melon-yellow@users.noreply.github.com>
2021-12-29 08:27:27 -0300

    Update reports.ex

--- homerico/lib/homerico_sx_web/controllers/reports.ex
+++ homerico/lib/homerico_sx_web/controllers/reports.ex
@@ -7,23 +7,16 @@ defmodule HomericoSxWeb.ReportsController do
       |> Enum.filter(&!String.contains?(&1, "!"))
   )
 
-  defmacro is_report(term) do
-    Enum.any?(@reports, &(term == &1))
-  end
-
   def handle(
     conn,
     %{"report" => report} = params
-  ) when is_report(report) do
+  ) when report in @reports do
     report
       |> &String.to_existing_atom/1
       |> execute!(params)
   end
 
-  defp execute!(
-    report,
-    params
-  ) when
+  defp execute!(report, params) when
     is_atom(report) and
     is_map(params)
   do
