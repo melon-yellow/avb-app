60729262afca34ec3a0ea1951f5301ace8f82521
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-01-03 17:28:40 -0300

    Update reports_controller.ex

--- homerico/lib/homerico_sx_web/controllers/reports_controller.ex
+++ homerico/lib/homerico_sx_web/controllers/reports_controller.ex
@@ -1,37 +1,30 @@
 defmodule HomericoSxWeb.ReportsController do
   use HomericoSxWeb, :controller
 
-  @reports_raw (
-    :functions
-      |> HomericoSx.Reports.__info__()
-      |> Enum.map(&elem(&1, 0))
+  @reports_raw (:functions
+    |> HomericoSx.Reports.__info__
+    |> Enum.map(&elem(&1, 0))
   )
-  @reports (
-    @reports_raw
-      |> Enum.map(&Atom.to_string/1)
+  @reports (@reports_raw
+    |> Enum.map(&Atom.to_string/1)
   )
 
   defp apply!(report, params) when
-    is_map(params) and
-    is_binary(report) and
-    report in @reports
+    is_map(params) and is_binary(report) and report in @reports
   do
     func = String.to_existing_atom report
     args = [HomericoSx.Connect.config!(), params]
     try do apply HomericoSx.Reports, func, args
-    rescue _ -> "invalid arguments"
     catch _ -> "invalid arguments"
     end
   end
 
-  defp apply!(report, params) when
-    is_map(params) and is_binary(report)
-  do "report not found" end
+  defp apply!(report, params)
+    when is_map(params) and is_binary(report), do:
+      "report not found"
 
-  def handle(
-    conn,
-    %{"report" => report} = params
-  ) when is_binary(report) do
-    html conn, (apply! report, params)
-  end
+  def handle(conn, %{"report" => report} = params)
+    when is_binary(report), do:
+      html conn, (apply! report, params)
+
 end
