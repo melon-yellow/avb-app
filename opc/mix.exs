5365d317dca4cd28152f880b7b6a132b69c85135
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-01-04 10:50:20 -0300

    add opc dep

--- opc/mix.exs
+++ opc/mix.exs
@@ -37,7 +37,8 @@ defmodule OpcSx.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:opex62541, git: "https://github.com/valiot/opex62541"}
     ]
   end
 
