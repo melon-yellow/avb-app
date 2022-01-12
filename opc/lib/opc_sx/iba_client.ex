5c8a66fb705d5e32c70bc5dd9b3ebca7a5926505
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-01-12 14:54:48 -0300

    using example cert

--- opc/lib/opc_sx/iba_client.ex
+++ opc/lib/opc_sx/iba_client.ex
@@ -10,8 +10,8 @@ defmodule OpcSx.IbaClient do
 
   defp cert_config!, do: [
     security_mode: 2,
-    certificate: read_cert!("opcuaviewer.der"),
-    private_key: read_cert!("opcuaviewer.pem")
+    certificate: read_cert!("client_cert.der"),
+    private_key: read_cert!("client_key.der")
   ]
 
   def start_link(_args) do
