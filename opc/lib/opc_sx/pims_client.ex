959264c1b0376d9b185cdf98a77e7485e0404d00
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-01-12 15:14:08 -0300

    Update pims_client.ex

--- opc/lib/opc_sx/pims_client.ex
+++ opc/lib/opc_sx/pims_client.ex
@@ -3,16 +3,25 @@ defmodule OpcSx.PimsClient do
 
   @pid :opc_sx_pims_client_pid
 
+  @config %{ns: 2, s: ""}
+
   def start_link(_args) do
-    {:ok, pid} = OpcUA.Client.start_link
-    :ok = OpcUA.Client.set_config pid
-    :ok = OpcUA.Client.connect_by_url pid, url: System.get_env("AVB_PIMS_OPC_ADDRESS")
-    Process.register pid, @pid
-    {:ok, pid}
+    try do
+      {:ok, pid} = OpcUA.Client.start_link
+      :ok = OpcUA.Client.set_config pid
+      :ok = OpcUA.Client.connect_by_url pid, url: System.get_env("AVB_PIMS_OPC_ADDRESS")
+      Process.register pid, @pid
+      {:ok, pid}
+    rescue reason -> {:error, reason}
+    catch reason -> {:error, reason}
+    end
   end
 
-  defp set_node!(id), do:
-    %OpcUA.NodeId{ns_index: 2, identifier_type: "string", identifier: id}
+  defp set_node!(id), do: %OpcUA.NodeId{
+    ns_index: @config.ns,
+    identifier_type: "string",
+    identifier: @config.s <> id
+  }
 
   defp read_node_value!(node_id), do:
     OpcUA.Client.read_node_value @pid, node_id
