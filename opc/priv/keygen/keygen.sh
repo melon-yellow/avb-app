98353e91f24cadc54b68dba38b571b09e158e99e
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-01-13 14:38:33 -0300

    Update keygen.sh

--- opc/priv/keygen/keygen.sh
+++ opc/priv/keygen/keygen.sh
@@ -4,12 +4,13 @@ NAME=elixir-client
 URI=urn:UAVBSRV:elixir-opex62541:avb-app
 
 SCRIPT=priv/keygen/create-self-signed.py
+SIZE=2048
 DEST=priv/certs
 CA=priv/certs/localhost.crt
 
 if [ ! -f "$CA" ]
 then
-    python3 "$SCRIPT" "$DEST" -u "$URI" -c "$NAME"
+    python3 "$SCRIPT" "$DEST" -u "$URI" -k "$SIZE" -c "$NAME"
 fi
 
 cp "$CA" /usr/local/share/ca-certificates/
