71e3e2cb8d962d90e3ce606cf861f476a80dd5bf
jayronhd <jayronhd@gmail.com>
2022-01-19 15:57:02 -0300

    Update trefila.py

--- mysql/modules/trefila.py
+++ mysql/modules/trefila.py
@@ -148,7 +148,8 @@ class metas:
             # Get Meta Parser
             day = getMetaDay(df, now)
             meta = getMetaTrim(df, now, metaTrimParser)
-            meta.update({ 'meta': 110, 'dia': day })
+            #meta.update({ 'meta': 110, 'dia': day }) jayron
+            meta.update({ 'dia': day })
             # Return Data
             return { 'custo': meta }
         # On Error
