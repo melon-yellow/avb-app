aacabb547eadb0292fbbad58adb11b954aab21cb
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-02-07 13:46:06 -0300

    Update client.ex

--- opc/lib/opc_sx/iba/client.ex
+++ opc/lib/opc_sx/iba/client.ex
@@ -10,6 +10,22 @@ defmodule OpcSx.Iba do
   def read_node_value(nid), do:
     OpcUA.Client.read_node_value IbaClient, nid
 
+  defp cert!(path), do: :opc_sx
+    |> Application.app_dir("priv/certs/#{path}")
+    |> File.read!
+
+  def config do
+    try do
+      config = [
+        security_mode: 3,
+        certificate: cert!("elixir-client_cert.der"),
+        private_key: cert!("elixir-client_key.der")
+      ]
+      {:ok, config}
+    catch _, reason -> {:error, reason}
+    end
+  end
+
 end
 
 ##########################################################################################################################
@@ -19,23 +35,14 @@ defmodule OpcSx.Iba.Client do
 
   def start_link(init_arg) when is_list(init_arg) do
     try do
+      {:ok, config} = OpcSx.Iba.config
       {:ok, pid} = OpcUA.Client.start_link(__MODULE__, init_arg)
-      :ok = OpcUA.Client.set_config_with_certs pid, cert_config!()
+      :ok = OpcUA.Client.set_config_with_certs pid, config
       :ok = OpcUA.Client.connect_by_url pid, url: System.get_env("AVB_IBA_OPC_URL")
       {:ok, pid}
     catch _, reason -> {:error, reason}
     end
   end
-
-  defp read_cert!(path), do: :opc_sx
-    |> Application.app_dir("priv/certs/#{path}")
-    |> File.read!
-
-  defp cert_config!, do: [
-    security_mode: 3,
-    certificate: read_cert!("elixir-client_cert.der"),
-    private_key: read_cert!("elixir-client_key.der")
-  ]
 
 end
 
