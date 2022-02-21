b13eafd2dbcb918874cb23810e1e9c21ba26e472
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-02-20 21:02:19 -0300

    fix config map

--- homerico/lib/homerico_sx/client.ex
+++ homerico/lib/homerico_sx/client.ex
@@ -13,13 +13,13 @@ end
 defmodule HomericoSx.Client do
   use Homerico.Client, restart: :transient, shutdown: 10_000
 
-  def configuration, do: [
+  def configuration, do: %{
     gateway: (System.get_env "HOMERICO_GATEWAY"),
-    login: [
+    login: %{
       user: (System.get_env "HOMERICO_USER"),
       password: (System.get_env "HOMERICO_PASSWORD")
-    ]
-  ]
+    }
+  }
 
 end
 
