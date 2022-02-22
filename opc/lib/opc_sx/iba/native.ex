ed21488bc6104708433779c7006109ab343819f9
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-02-22 02:21:31 -0300

    split http client from native

--- opc/lib/opc_sx/iba/native.ex
+++ opc/lib/opc_sx/iba/native.ex
@@ -4,26 +4,38 @@
 defmodule OpcSx.Iba.IoConfig do
   use Unsafe.Generator, handler: {Unsafe.Handler, :bang!}
   use Rustler, otp_app: :opc_sx, crate: :io_config
+  alias OpcSx.Iba.IoConfig.Network
 
   @unsafe [read: 0]
+
+  def parse(_), do: :erlang.nif_error(:nif_not_loaded)
+
+  def read do
+    try do
+      io = Network.get |> parse()
+      {:ok, io}
+    catch _, reason -> {:error, reason}
+    end
+  end
+
+end
+
+##########################################################################################################################
+
+defmodule OpcSx.Iba.IoConfig.Network do
 
   @request Finch.build(:get,
     "https://raw.githubusercontent.com" <>
     "/melon-yellow/avb-iba/main/pda/pda.config.io"
   )
 
-  defp read_xml, do:
-    Finch.request @request, HTTPClient
+  defp handle_http!({:ok, %{status_code: 200, body: body}}), do: handle_data!(body)
+  defp handle_http!({:ok, %{status_code: 404}}), do: throw "(404) could not reach the link"
+  defp handle_http!({:error, %{reason: reason}}), do: throw reason
 
-  def parse_config(_), do: :erlang.nif_error(:nif_not_loaded)
-
-  def read do
-    try do
-      io = read_xml |> parse_config
-      {:ok, io}
-    catch _, reason -> {:error, reason}
-    end
-  end
+  def get, do: @request
+    |> Finch.request(HTTPClient)
+    |> handle_http!()
 
 end
 
