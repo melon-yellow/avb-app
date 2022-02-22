49269f3ea931556e3f00ebd9b9620fe25c0edae7
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-02-22 03:30:49 -0300

    Update mix.exs

--- opc/mix.exs
+++ opc/mix.exs
@@ -4,7 +4,7 @@ defmodule OpcSx.MixProject do
   def project do
     [
       app: :opc_sx,
-      version: "0.1.0",
+      version: "0.1.3",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
